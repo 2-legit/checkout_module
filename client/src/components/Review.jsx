e6960ae3132842cd1ce6d5be76b70ee40b59d748
Paul Diamond <pfdiamond13@gmail.com>
2019-04-03 15:31:24 -0700

    Updates review with style/dynamically render

---
+++ client/src/components/Review.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ReviewWrapper = styled.section`
+  overflow-wrap: break-word;
+  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
+  font-size: 12px;
+  font-weight: 600;
+  line-height: 1.33333em;
+  color: rgb(72, 72, 72);
+  margin: 0px;
+  float: left;
+`;
+
+
+const Review = (props) => {
+  return (
+    <div>
+      <ReviewWrapper>
+        Reviews {props.room.reviews}
+      </ReviewWrapper>
+    </div>
+  );
+};
+
+
+export default Review;
