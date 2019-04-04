a270fd632f36d90951e591253669225c8e21d8ba
Paul Diamond <pfdiamond13@gmail.com>
2019-04-03 18:02:56 -0700

    Refactor CSS

--- client/src/components/Cost.jsx
+++ client/src/components/Cost.jsx
@@ -2,35 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CostWrapper = styled.section`
-  float:left;
+  display: flex;
   font-size: 22px;
   font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
   font-weight: 800;
   color: rgb(72, 72, 72);
   overflow-wrap: break-word;
-  line-height: 1.44444em; 
+  line-height: 1.44444em;
+  flex-direction: row; 
 `;
 
 const NightWrapper = styled.section`
-  float: left;
+  display: flex;
   font-size: 12px !important;
   font-weight: 600 !important;
+  flex-direction: row;
+  padding-left: 5px;
 `;
 
 const Cost = (props) => {
   return (
-    <div>
+    <CostWrapper>
       <span>
-        <CostWrapper>
           ${props.room.cost}
-        </CostWrapper>
       </span>
       <span>
         <NightWrapper>
           per night
         </NightWrapper>
       </span>
-    </div>
+    </CostWrapper>
   );
 };
 
