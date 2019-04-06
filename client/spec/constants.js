098041e6bf2e22a008a8e42a1c97336ece2bf9be
Paul Diamond <pfdiamond13@gmail.com>
2019-04-05 21:35:19 -0700

    Constants for tests

---
+++ client/spec/constants.js
@@ -0,0 +1,11 @@
+export const MOCK_ROOM = {
+  id: 1,
+  room_id: 1,
+  booked_date: '2019-05-15',
+  createdAt: '2019-03-29T17:04:47.000Z',
+  updatedAt: '2019-03-29T17:04:47.000Z',
+  cost: 215,
+  reviews: 89816,
+  avg_review: '4',
+  guests: 4,
+};
