acff7c79747a539ff5842c1ac33a3588e3e07d94
Paul Diamond <pfdiamond13@gmail.com>
2019-04-05 21:34:35 -0700

    Refactor to use Mock Data and mock server

--- client/spec/Checkout.test.js
+++ client/spec/Checkout.test.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import axios from 'axios';
-
+import { MOCK_ROOM } from './constants';
 
 import Checkout from '../src/components/Checkout';
 import Cost from '../src/components/Cost';
-// jest.mock('axios');
-// needs to be in this scope, but in this scope breaks other tests
+
+const LOADED_PROPS = {
+  isLoaded: true,
+  room: [MOCK_ROOM],
+};
 
 describe('Checkout Test Suite', () => {
 
   it('Should render', () => {
-    const wrapper = shallow(<Checkout />);
+    const wrapper = shallow(<Checkout {...LOADED_PROPS} />);
     expect(wrapper.exists()).toBe(true);
   });
 
-  xit('Should have one <Cost /> component', () => {
-    const wrapper = mount(<Checkout />);
+  it('Should have one <Cost /> component', () => {
+    const wrapper = mount(<Checkout {...LOADED_PROPS} />);
     expect(wrapper.find(Cost)).toHaveLength(1);
   });
 
-  xit('Should fetch rooms', () => {
-    const room = [{roomId: '1'}];
-    const resp = {data: room};
-    axios.get.mockImplementation(() => Promise.resolve(resp));
+  it('Should render loading placeholder if isLoaded is false', () => {
+    const wrapper = mount(<Checkout  isLoaded={false} />);
+    expect(wrapper.find('div').text()).toBe('Loading...');
   });
-
 });
