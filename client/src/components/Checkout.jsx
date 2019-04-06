a4172d5bb2383066c26f9b74ccc66a3674cf4e34
Paul Diamond <pfdiamond13@gmail.com>
2019-04-05 21:38:10 -0700

    Refactor to not hold componentDidMount

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import axios from 'axios';
 import styled from 'styled-components';
 import Review from './Review';
 import Cost from './Cost';
 import Book from './Book';
+import Calendar from './Calendar';
+import CheckInOut from './CheckInOut';
+import Guests from './Guests';
 
 
 const Wrapper = styled.section`
@@ -17,36 +19,51 @@ const Wrapper = styled.section`
 `;
 
 class Checkout extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      room: [],
-      isLoaded: false,
+      checkIn: null,
+      checkOut: null,
+      guestCount: 1,
     };
   }
 
-  componentDidMount() {
-    axios.get('/rooms/1/reservations') // hard coded TODO refactor to be dynamic
-      .then(response => (
-        response.data
-      ))
-      .then((roomData) => {
-        this.setState({
-          isLoaded: true,
-          room: roomData[0],
-        });
-      });
+  setCheckIn(value) {
+    if (this.state.checkIn) {
+      this.setState({ checkOut: value });
+    } else {
+      this.setState({ checkIn: value });
+    }
+  }
+
+  addGuest() {
+    if (this.state.guestCount < this.props.room.guests) {
+      this.setState({ guestCount: this.state.guestCount + 1 });
+    }
+  }
+
+  subGuest() {
+    console.log(this.state.guestCount)
+    if (this.state.guestCount > 1) {
+      this.setState({ guestCount: this.state.guestCount - 1 });
+    }
   }
 
   render() {
-    if (!this.state.isLoaded) {
+    const { isLoaded, room } = this.props;
+    const { checkIn, checkOut, guestCount } = this.state;
+
+    if (!isLoaded) {
       return <div>Loading...</div>;
     }
     return (
       <Wrapper>
-        <div><Cost room={this.state.room[0]}/></div>
-        <div><Review room={this.state.room[0]}/></div>
-        <div><Book room={this.state.room[0]}/></div>
+        <div><Cost room={room} /></div>
+        <div><Review room={room} /></div>
+        <div><CheckInOut checkIn={checkIn} checkOut={checkOut} /></div>
+        <div><Calendar room={room} setCheckIn={this.setCheckIn.bind(this)}/></div>
+        <div><Guests guestCount={guestCount} addGuest={this.addGuest.bind(this)} subGuest={this.subGuest.bind(this)} /></div>
+        <div><Book room={room} /></div>
       </Wrapper>
     );
   }
