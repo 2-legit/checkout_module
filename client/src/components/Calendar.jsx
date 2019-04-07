8a2d3dc5e3199cfa3990781a4b35b669b777d384
Paul Diamond <pfdiamond13@gmail.com>
2019-04-06 21:35:11 -0700

    Refactor into seperate components

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,29 +1,8 @@
 import React from 'react';
 import moment from 'moment';
 import styled from 'styled-components';
-
-
-const BookedDayWrapper = styled.td`
-  color: grey;
-  border: 1px solid #e4e4e4;
-  text-decoration: line-through;
-  font-size: 14px;
-  text-align: center;
-  font-weight: 600;
-`;
-
-const VacantDayWrapper = styled.td`
-  color: rgb(72, 72, 72);
-  border: 1px solid #e4e4e4;
-  font-size: 14px;
-  cursor: pointer;
-  text-align: center;
-  font-weight: 600;
-`;
-
-const TableWrapper = styled.table`
-  table-layout: 'fixed';
-`;
+import Months from './Months';
+import Days from './Days';
 
 const CalendarWrapper = styled.section`
   display: flex;
@@ -31,25 +10,6 @@ const CalendarWrapper = styled.section`
   align-items: center;
   justify-content: center;
 `;
-
-const MonthWrapper = styled.section`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif;
-  color: rgb(72, 72, 72) !important;
-  font-size: 18px !important;
-  text-align: center !important;
-  font-weight: bold;
-`;
-
-const WeekDayWrapper = styled.thead`
-  color: rgb(117, 117, 117);
-  font-size: 14px;
-  font-weight: 600;
-`;
-
 
 class Calendar extends React.Component {
   constructor(props) {
@@ -59,9 +19,26 @@ class Calendar extends React.Component {
     };
   }
 
+
+  setNextMonth() {
+    let dateObject = Object.assign({}, this.state.dateObject);
+    dateObject = moment(dateObject).add(1, 'month');
+    this.setState({
+      dateObject: dateObject,
+    });
+  }
+
+  setPrevMonth() {
+    let dateObject = Object.assign({}, this.state.dateObject);
+    dateObject = moment(dateObject).add(-1, 'month');
+    this.setState({
+      dateObject: dateObject,
+    });
+  }
+
   firstDayOfMonth() {
     let dateObject = this.state.dateObject;
-    let firstDay = moment(dateObject).startOf('month').format('d');
+    const firstDay = moment(dateObject).startOf('month').format('d');
     return firstDay;
   }
 
@@ -77,98 +54,25 @@ class Calendar extends React.Component {
     return this.state.dateObject.format('YYYY');
   }
 
-  setNextMonth() {
-    let dateObject = Object.assign({}, this.state.dateObject);
-    dateObject = moment(dateObject).add(1, 'month');
-    this.setState({
-      dateObject: dateObject,
-    });
-  }
-  setPrevMonth() {
-    let dateObject = Object.assign({}, this.state.dateObject);
-    dateObject = moment(dateObject).add(-1, 'month');
-    this.setState({
-      dateObject: dateObject,
-    });
-  }
-
-  setDay(data) {
-    console.log(this.state.dateObject.format('YY-MM-' + data).toString());
-  }
-
   render() {
-    const weekDayName = moment.weekdaysShort();
-    const weekDays = weekDayName.map(day => {
-      return <th key={day} className='weekday'>{day}
-      </th>;
-    });
-
-    const blankDays = [];
-    for (let i = 0; i < this.firstDayOfMonth(); i++) {
-      blankDays.push(<td className="calendar-day empty">{""}</td>,
-      );
-    }
-
-    const daysInMonth = [];
-    for (let d = 1; d <= this.totalDaysInMonth(); d++) {
-
-      if (d < 10) {
-        if (this.props.room.bookedDates.indexOf(this.state.dateObject.format('YYYY-MM-' + 0 + d).toString()) === -1) {
-          daysInMonth.push(
-            <VacantDayWrapper><td key={'0' + d} className="calendarDay" id='vacant' onClick={e => { this.props.setCheckIn(this.state.dateObject.format( 'MM-' + 0 + d +'-YYYY').toString()) }}>
-              {d}
-            </td></VacantDayWrapper>,
-          );
-        } else {
-          daysInMonth.push(
-            <BookedDayWrapper><td key={'0' + d} className="calendarDay" id='booked' onClick={e => { this.setDay('0' + d) }}>
-              {d}
-            </td></BookedDayWrapper>,
-          );
-        }
-      } else {
-        if (this.props.room.bookedDates.indexOf(this.state.dateObject.format('YYYY-MM-' + d).toString()) === -1) {
-          daysInMonth.push(
-            <VacantDayWrapper><td key={d} className="calendarDay" id='vacant' onClick={e => { this.props.setCheckIn(this.state.dateObject.format( 'MM-' + d +'-YYYY').toString()) }}>
-              {d}
-            </td></VacantDayWrapper>,
-          );
-        } else {
-          daysInMonth.push(
-            <BookedDayWrapper><td key={d} className="calendarDay" id='booked' onClick={e => { this.setDay(d.toString()) }}>
-              {d}
-            </td></BookedDayWrapper>,
-          );
-        }
-      }
-    }
-
-    let totalSlots = [...blankDays, ...daysInMonth];
-    let rows = [];
-    let cells = [];
-    totalSlots.forEach((row, i) => {
-      if (i % 7 !== 0) {
-        cells.push(row);
-      } else {
-        rows.push(cells);
-        cells = [];
-        cells.push(row);
-      }
-      if (i === totalSlots.length - 1) {
-        rows.push(cells);
-      }
-    });
-    let daysinmonth = rows.map((d, i) => {
-      return <tr>{d}</tr>;
-    });
     return (
-      <CalendarWrapper><div>
-        <MonthWrapper><button onClick={e => { this.setPrevMonth() }}>prev</button><div>{this.currentMonth()} {this.currentYear()}</div><button onClick={e => { this.setNextMonth() }}>next</button></MonthWrapper>
-        <TableWrapper>
-          <WeekDayWrapper><tr>{weekDays}</tr></WeekDayWrapper>
-          <tbody>{daysinmonth}</tbody>
-        </TableWrapper>
-      </div>
+      <CalendarWrapper>
+        <div>
+          <Months
+            currentMonth={this.currentMonth.bind(this)}
+            currentYear={this.currentYear.bind(this)}
+            setNextMonth={this.setNextMonth.bind(this)}
+            setPrevMonth={this.setPrevMonth.bind(this)}
+          />
+          <Days
+            firstDayOfMonth={this.firstDayOfMonth.bind(this)}
+            totalDaysInMonth={this.totalDaysInMonth.bind(this)}
+            setCheckIn={this.props.setCheckIn}
+            setCheckOut={this.props.setCheckOut}
+            room={this.props.room}
+            dateObject={this.state.dateObject}
+          />
+        </div>
       </CalendarWrapper>
     );
   }
---
+++ client/src/components/Months.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MonthWrapper = styled.section`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif;
+  color: rgb(72, 72, 72) !important;
+  font-size: 18px !important;
+  text-align: center !important;
+  font-weight: bold;
+`;
+
+const PrevMonthButton = styled.button`
+`;
+
+const Months = (props) => {
+  return (
+    <MonthWrapper>
+      <PrevMonthButton type="button" onClick={() => (props.setPrevMonth())}>&lt;</PrevMonthButton>
+      <div>
+        &nbsp;
+        {props.currentMonth()}
+        &nbsp;
+        {props.currentYear()}
+        &nbsp;
+      </div>
+      <button type="button" onClick={() => (props.setNextMonth())}>&gt;</button>
+    </MonthWrapper>
+  );
+};
+
+export default Months;
